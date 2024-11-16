17be6f37429c1ef7daf328371192127bbd2456f9
uemerson199 <uemersonribeiro220@gmail.com>
2024-11-15 22:13:55 -0300

    H2 database

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 

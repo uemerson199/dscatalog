154c40396f791ca266a594eabd5a405e33b40e11
uemerson199 <uemersonribeiro220@gmail.com>
2024-11-18 15:41:08 -0300

    Pagination

--- backend/dscatalog/src/main/resources/import.sql
+++ backend/dscatalog/src/main/resources/import.sql
@@ -2,3 +2,35 @@ INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
 INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
 INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
 
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+
+INSERT INTO tb_category(name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
+

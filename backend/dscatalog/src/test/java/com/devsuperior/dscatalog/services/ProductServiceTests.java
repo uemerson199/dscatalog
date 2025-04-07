d171a38d50bef0592ba92e863be799a5e4283c86
uemerson199 <uemersonribeiro220@gmail.com>
2025-04-07 17:28:47 -0300

    Começando testes de ProductService, Mockito vs MockBean

---
+++ backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+
+    @InjectMocks
+    private ProductService service;
+
+
+    @Mock
+    private ProductRepository repository;
+
+    @MockBean
+    private ProductRepository repository2;
+
+}
+
+
+
